255bb3f4ad9fcc8c73d6b7a77a0d7718e0043fdf
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-11-16 19:57:17 +0200

    Basket Entities

---
+++ ECommerceDomain/Entities/BasketModule/CustomerBasket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceDomain.Entities.BasketModule
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; } = default!;
+        public ICollection<BasketItem> Items { get; set; } =[];
+
+    }
+}
