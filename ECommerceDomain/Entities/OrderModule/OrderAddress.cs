5bc663b4c09efe9a2f97d129ebcf17ca7f55ffb6
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-11-29 19:03:51 +0200

    Order Entities

---
+++ ECommerceDomain/Entities/OrderModule/OrderAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceDomain.Entities.OrderModule
+{
+    public class OrderAddress
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Street { get; set; } = null!; 
+        public string City { get; set; } = null!;
+
+        public string Country { get; set; } = null!;
+      
+    }
+}
